9aacd366718fbb05271f3011d945e3da8e2d4e41
oliik2013 <131269027+oliik2013@users.noreply.github.com>
2025-04-17 21:28:14 +0200

    Added /olm for checking plugin version

--- src/main/java/com/oliik2013/oliiksloginmsgs.java
+++ src/main/java/com/oliik2013/oliiksloginmsgs.java
@@ -1,6 +1,13 @@
 package com.oliik2013;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.BlockCommandSender;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public final class oliiksloginmsgs extends JavaPlugin {
 
@@ -11,5 +18,22 @@ public final class oliiksloginmsgs extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new OLMListener(), this);
     }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("olm")){
+            if (sender instanceof Player) {
+                Player p = (Player) sender;
+                p.sendMessage(ChatColor.AQUA + "Running Oliiks Login Msgs version: " + ChatColor.DARK_BLUE + getDescription().getVersion());
+            }else if(sender instanceof ConsoleCommandSender){
+                System.out.println(ChatColor.AQUA + "Running Oliiks Login Msgs version: " + ChatColor.DARK_BLUE + getDescription().getVersion());
+
+
+            }else if(sender instanceof BlockCommandSender){
+                System.out.println(ChatColor.AQUA + "Running Oliiks Login Msgs version: " + ChatColor.DARK_BLUE + getDescription().getVersion());
+            }
+        }
+
+        return true;
+    }
 
 }

23f700dc2ae2d3bbdb571246eb60a89b7d57ff65
oliik2013 <131269027+oliik2013@users.noreply.github.com>
2025-04-18 15:34:51 +0200

    Added commands for changing the messages

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 An simple login msgs spigot plugin.
 
 How to use:
-    To change the messages, change the config.yml of this plugin.
+    To change the messages, change the config.yml of this plugin or use an command listed below.
 
 Commands:
+    /olm setjoinmessage - sets the join message
+    /olm setfirstjoinmessage - sets the first time join message
+    /olm setleavemessage - sets the leave message
     /olm - See the plugin version.
     /oliiksloginmsgs - Alias for /olm
 
--- src/main/java/com/oliik2013/oliiksloginmsgs.java
+++ src/main/java/com/oliik2013/oliiksloginmsgs.java
@@ -8,6 +8,8 @@ import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 
 public final class oliiksloginmsgs extends JavaPlugin {
 
@@ -23,19 +25,67 @@ public final class oliiksloginmsgs extends JavaPlugin {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("olm")){
-            if (sender instanceof Player) {
-                Player p = (Player) sender;
-                p.sendMessage(ChatColor.AQUA + "Running Oliiks Login Msgs version: " + ChatColor.DARK_BLUE + getDescription().getVersion());
-            }else if(sender instanceof ConsoleCommandSender){
-                System.out.println(ChatColor.AQUA + "Running Oliiks Login Msgs version: " + ChatColor.DARK_BLUE + getDescription().getVersion());
+            if(args.length == 0) {
+                if (sender instanceof Player) {
+                    Player p = (Player) sender;
+                    p.sendMessage(ChatColor.AQUA + "Running Oliiks Login Msgs version: " + ChatColor.DARK_BLUE + getDescription().getVersion());
+                } else if (sender instanceof ConsoleCommandSender) {
+                    System.out.println(ChatColor.AQUA + "Running Oliiks Login Msgs version: " + ChatColor.DARK_BLUE + getDescription().getVersion());
 
 
-            }else if(sender instanceof BlockCommandSender){
-                System.out.println(ChatColor.AQUA + "Running Oliiks Login Msgs version: " + ChatColor.DARK_BLUE + getDescription().getVersion());
+                } else if (sender instanceof BlockCommandSender) {
+                    System.out.println(ChatColor.AQUA + "Running Oliiks Login Msgs version: " + ChatColor.DARK_BLUE + getDescription().getVersion());
+
+                }
+
+            }else if (Objects.equals(args[0], "setjoinmessage")){
+                StringBuilder builder = new StringBuilder();
+
+
+
+                for(int i = 1; i < args.length; i++){
+                    builder.append(args[i]);
+                    builder.append(" ");
+                }
+
+                String finalMessage = builder.toString();
+                finalMessage = finalMessage.stripTrailing();
+                getConfig().set("join-message", finalMessage);
+                saveConfig();
+
+            }else if (Objects.equals(args[0], "setfirstjoinmessage")) {
+                StringBuilder builder = new StringBuilder();
+
+
+                for (int i = 1; i < args.length; i++) {
+                    builder.append(args[i]);
+                    builder.append(" ");
+                }
+
+                String finalMessage = builder.toString();
+                finalMessage = finalMessage.stripTrailing();
+                getConfig().set("frist-join-message", finalMessage);
+                saveConfig();
+            }else if (Objects.equals(args[0], "setleavemessage")) {
+                StringBuilder builder = new StringBuilder();
+
+
+                for (int i = 1; i < args.length; i++) {
+                    builder.append(args[i]);
+                    builder.append(" ");
+                }
+
+                String finalMessage = builder.toString();
+                finalMessage = finalMessage.stripTrailing();
+                getConfig().set("leave-message", finalMessage);
+                saveConfig();
             }
         }
 
         return true;
     }
+
+
+
 
 }
